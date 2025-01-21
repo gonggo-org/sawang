203c64ceeadf108abf336b327f1eb88bb151831f
AbdulYadi <abdulyadi.datatrans@gmail.com>
2025-01-21 07:55:58 +0700

    add note in callback.h

--- gear/callback.h
+++ gear/callback.h
@@ -13,13 +13,6 @@ enum ProxyPayloadParseResult {
     PARSE_MULTIRESPOND = 3
 };
 
-typedef enum ProxyPayloadParseResult (*ProxyPayloadParse) (
-    const char *service_name, 
-    const cJSON *payload, 
-    cJSON **normalized_payload, 
-    bool *unsubscribe,
-    unsigned int *invalid_status);
-
 typedef struct ProxyReplyArg {
     char *service;
     cJSON *payload; 
@@ -27,6 +20,21 @@ typedef struct ProxyReplyArg {
 
 typedef void (*ProxyReply) (const ProxyReplyArg *arg, cJSON *headers, cJSON *payload);
 typedef void (*ProxyFree) (ProxyReplyArg *arg);
+
+/*
+ * To be implemented by customized proxy as required by work(...) defined in work.h
+ * 1. ProxyPayloadParse: runs in proxychannel thread on client request parsing.
+ * 2. ProxyStart: runs in proxy proxycomm thread start. A function to initialize resources.
+ * 3. ProxyRun: runs in proxycomm loop.
+ * 4. ProxyMultiRespondClear: runs in proxycomm loop to clear a multirespond service.
+ * 5. ProxyStop: run in proxycomm thread stop. A function to destroy resources.
+ */
+typedef enum ProxyPayloadParseResult (*ProxyPayloadParse) (
+    const char *service_name, 
+    const cJSON *payload, 
+    cJSON **normalized_payload, 
+    bool *unsubscribe,
+    unsigned int *invalid_status);
 typedef void (*ProxyStart) (void);
 typedef void (*ProxyRun) (ProxyReplyArg *arg, ProxyReply f_proxy_reply, ProxyFree f_proxy_free);
 typedef void (*ProxyMultiRespondClear) (ProxyReplyArg *arg, ProxyFree f_proxy_free);
